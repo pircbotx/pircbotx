f4a9e0b849ea4be50f4995f64eb834171a22c989
lord quackstar <lord.quackstar@gmail.com>
2010-08-23 01:35:03 +0000

    Updated license headers on all files. Deleted PerformanceTest, no longer needed. Removed target folder until a better solution can be found

--- src/main/java/org/pircbotx/DccFileTransfer.java
+++ src/main/java/org/pircbotx/DccFileTransfer.java
@@ -1,19 +1,22 @@
-/*
- * Copyright (C) 2009-2010 Leon Blakey
+/**
+ * Copyright (C) 2010 Leon Blakey <lord.quackstar at gmail.com>
  *
- * Quackedbot is free software: you can redistribute it and/or modify
+ * This file is part of PircBotX.
+ *
+ * PircBotX is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Quackedbot  is distributed in the hope that it will be useful,
+ * PircBotX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 package org.pircbotx;
 
 import java.io.BufferedInputStream;
