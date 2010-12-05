554b6aef9c49d4ce6ede6841fcbdfa9593f94452
lord quackstar <lord.quackstar@gmail.com>
2010-12-05 23:14:15 +0000

    Propagate rename to lowercase to all files

--- src/main/java/org/pircbotx/hooks/helpers/BaseEvent.java
+++ src/main/java/org/pircbotx/hooks/helpers/BaseEvent.java
@@ -1,20 +1,20 @@
-/**
+/*
  * Copyright (C) 2010 Leon Blakey <lord.quackstar at gmail.com>
  *
- * This file is part of PircBotX.
+ * This file is part of pircbotx.
  *
- * PircBotX is free software: you can redistribute it and/or modify
+ * pircbotx is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * PircBotX is distributed in the hope that it will be useful,
+ * pircbotx is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
+ * along with pircbotx.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.pircbotx.hooks.helpers;
