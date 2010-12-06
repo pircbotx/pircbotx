c78b31cb721ae60cbe44647d35228ec33b048210
lord.quackstar@gmail.com <lord.quackstar@gmail.com>
2010-12-06 00:48:32 +0000

    Reverted to correct case PircBotX in License headers

--- deploy.bat
+++ deploy.bat
@@ -1,20 +1,20 @@
 @REM
 @REM Copyright (C) 2010 Leon Blakey <lord.quackstar at gmail.com>
 @REM
-@REM This file is part of pircbotx.
+@REM This file is part of PircBotX.
 @REM
-@REM pircbotx is free software: you can redistribute it and/or modify
+@REM PircBotX is free software: you can redistribute it and/or modify
 @REM it under the terms of the GNU General Public License as published by
 @REM the Free Software Foundation, either version 3 of the License, or
 @REM (at your option) any later version.
 @REM
-@REM pircbotx is distributed in the hope that it will be useful,
+@REM PircBotX is distributed in the hope that it will be useful,
 @REM but WITHOUT ANY WARRANTY; without even the implied warranty of
 @REM MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 @REM GNU General Public License for more details.
 @REM
 @REM You should have received a copy of the GNU General Public License
-@REM along with pircbotx.  If not, see <http://www.gnu.org/licenses/>.
+@REM along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
 @REM
 
 @echo off
--- src/main/java/org/pircbotx/impl/PircBotXTest.java
+++ src/main/java/org/pircbotx/impl/PircBotXTest.java
@@ -1,20 +1,20 @@
 /**
  * Copyright (C) 2010 Leon Blakey <lord.quackstar at gmail.com>
  *
- * This file is part of pircbotx.
+ * This file is part of PircBotX.
  *
- * pircbotx is free software: you can redistribute it and/or modify
+ * PircBotX is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * pircbotx is distributed in the hope that it will be useful,
+ * PircBotX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with pircbotx.  If not, see <http://www.gnu.org/licenses/>.
+ * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.pircbotx.impl;
