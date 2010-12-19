d2d122bbf53be690749c258c1a81347fd132dbbd
Leon Blakey <lord.quackstar@gmail.com>
2010-12-19 18:53:42 -0500

    Implmented Listeners throughout Adapters

--- src/main/java/org/pircbotx/hooks/helpers/ListenerAdapter.java
+++ src/main/java/org/pircbotx/hooks/helpers/ListenerAdapter.java
@@ -16,212 +16,211 @@
  * You should have received a copy of the GNU General Public License
  * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.pircbotx.hooks.helpers;
 
-import org.pircbotx.hooks.Action;
-import org.pircbotx.hooks.ChannelInfo;
-import org.pircbotx.hooks.Connect;
-import org.pircbotx.hooks.DeVoice;
-import org.pircbotx.hooks.Deop;
-import org.pircbotx.hooks.Disconnect;
-import org.pircbotx.hooks.FileTransferFinished;
-import org.pircbotx.hooks.Finger;
-import org.pircbotx.hooks.IncomingChatRequest;
-import org.pircbotx.hooks.IncomingFileTransfer.Event;
-import org.pircbotx.hooks.Invite;
-import org.pircbotx.hooks.Join;
-import org.pircbotx.hooks.Kick;
-import org.pircbotx.hooks.Message;
-import org.pircbotx.hooks.Mode;
-import org.pircbotx.hooks.Motd;
-import org.pircbotx.hooks.NickChange;
-import org.pircbotx.hooks.Notice;
-import org.pircbotx.hooks.Op;
-import org.pircbotx.hooks.Part;
-import org.pircbotx.hooks.Ping;
-import org.pircbotx.hooks.PrivateMessage;
-import org.pircbotx.hooks.Quit;
-import org.pircbotx.hooks.RemoveChannelBan;
-import org.pircbotx.hooks.RemoveChannelKey;
-import org.pircbotx.hooks.RemoveChannelLimit;
-import org.pircbotx.hooks.RemoveInviteOnly;
-import org.pircbotx.hooks.RemoveModerated;
-import org.pircbotx.hooks.RemoveNoExternalMessages;
-import org.pircbotx.hooks.RemovePrivate;
-import org.pircbotx.hooks.RemoveSecret;
-import org.pircbotx.hooks.RemoveTopicProtection;
-import org.pircbotx.hooks.ServerPing;
-import org.pircbotx.hooks.ServerResponse;
-import org.pircbotx.hooks.SetChannelBan;
-import org.pircbotx.hooks.SetChannelKey;
-import org.pircbotx.hooks.SetChannelLimit;
-import org.pircbotx.hooks.SetInviteOnly;
-import org.pircbotx.hooks.SetModerated;
-import org.pircbotx.hooks.SetNoExternalMessages;
-import org.pircbotx.hooks.SetPrivate;
-import org.pircbotx.hooks.SetSecret;
-import org.pircbotx.hooks.SetTopicProtection;
-import org.pircbotx.hooks.Time;
-import org.pircbotx.hooks.Topic;
-import org.pircbotx.hooks.Unknown;
-import org.pircbotx.hooks.UserList;
-import org.pircbotx.hooks.UserMode;
-import org.pircbotx.hooks.Version;
-import org.pircbotx.hooks.Voice;
+import org.pircbotx.events.ActionEvent;
+import org.pircbotx.events.ChannelInfoEvent;
+import org.pircbotx.events.ConnectEvent;
+import org.pircbotx.events.DeVoiceEvent;
+import org.pircbotx.events.DeopEvent;
+import org.pircbotx.events.DisconnectEvent;
+import org.pircbotx.events.FileTransferFinishedEvent;
+import org.pircbotx.events.FingerEvent;
+import org.pircbotx.events.IncomingChatRequestEvent;
+import org.pircbotx.events.IncomingFileTransferEvent;
+import org.pircbotx.events.InviteEvent;
+import org.pircbotx.events.JoinEvent;
+import org.pircbotx.events.KickEvent;
+import org.pircbotx.events.MessageEvent;
+import org.pircbotx.events.ModeEvent;
+import org.pircbotx.events.MotdEvent;
+import org.pircbotx.events.NickChangeEvent;
+import org.pircbotx.events.NoticeEvent;
+import org.pircbotx.events.OpEvent;
+import org.pircbotx.events.PartEvent;
+import org.pircbotx.events.PingEvent;
+import org.pircbotx.events.PrivateMessageEvent;
+import org.pircbotx.events.QuitEvent;
+import org.pircbotx.events.RemoveChannelBanEvent;
+import org.pircbotx.events.RemoveChannelKeyEvent;
+import org.pircbotx.events.RemoveChannelLimitEvent;
+import org.pircbotx.events.RemoveInviteOnlyEvent;
+import org.pircbotx.events.RemoveModeratedEvent;
+import org.pircbotx.events.RemoveNoExternalMessagesEvent;
+import org.pircbotx.events.RemovePrivateEvent;
+import org.pircbotx.events.RemoveSecretEvent;
+import org.pircbotx.events.RemoveTopicProtectionEvent;
+import org.pircbotx.events.ServerPingEvent;
+import org.pircbotx.events.ServerResponseEvent;
+import org.pircbotx.events.SetChannelBanEvent;
+import org.pircbotx.events.SetChannelKeyEvent;
+import org.pircbotx.events.SetChannelLimitEvent;
+import org.pircbotx.events.SetInviteOnlyEvent;
+import org.pircbotx.events.SetModeratedEvent;
+import org.pircbotx.events.SetNoExternalMessagesEvent;
+import org.pircbotx.events.SetPrivateEvent;
+import org.pircbotx.events.SetSecretEvent;
+import org.pircbotx.events.SetTopicProtectionEvent;
+import org.pircbotx.events.TimeEvent;
+import org.pircbotx.events.TopicEvent;
+import org.pircbotx.events.UnknownEvent;
+import org.pircbotx.events.UserListEvent;
+import org.pircbotx.events.UserModeEvent;
+import org.pircbotx.events.VersionEvent;
+import org.pircbotx.events.VoiceEvent;
 
 /**
  * TODO
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public class ListenerAdapter implements ListenerAdapterInterface {
-	public void onAction(Action.Event event) {
+	public void onAction(ActionEvent event) {
 	}
 
-	public void onChannelInfo(ChannelInfo.Event event) {
+	public void onChannelInfo(ChannelInfoEvent event) {
 	}
 
-	public void onConnect(Connect.Event event) {
+	public void onConnect(ConnectEvent event) {
 	}
 
-	public void onDeop(Deop.Event event) {
+	public void onDeop(DeopEvent event) {
 	}
 
-	public void onDeVoice(DeVoice.Event event) {
+	public void onDeVoice(DeVoiceEvent event) {
 	}
 
-	public void onDisconnect(Disconnect.Event event) {
+	public void onDisconnect(DisconnectEvent event) {
 	}
 
-	public void onFileTransferFinished(FileTransferFinished.Event event) {
+	public void onFileTransferFinished(FileTransferFinishedEvent event) {
 	}
 
-	public void onFinger(Finger.Event event) {
+	public void onFinger(FingerEvent event) {
 	}
 
-	public void onIncomingChatRequest(IncomingChatRequest.Event event) {
+	public void onIncomingChatRequest(IncomingChatRequestEvent event) {
 	}
 
-	public void onInvite(Invite.Event event) {
+	public void onIncomingFileTransfer(IncomingFileTransferEvent event) {
 	}
 
-	public void onJoin(Join.Event event) {
+	public void onInvite(InviteEvent event) {
 	}
 
-	public void onKick(Kick.Event event) {
+	public void onJoin(JoinEvent event) {
 	}
 
-	public void onMessage(Message.Event event) {
+	public void onKick(KickEvent event) {
 	}
 
-	public void onMode(Mode.Event event) {
+	public void onMessage(MessageEvent event) {
 	}
 
-	public void onNickChange(NickChange.Event event) {
+	public void onMode(ModeEvent event) {
 	}
 
-	public void onNotice(Notice.Event event) {
+	public void onMotd(MotdEvent event) {
 	}
 
-	public void onOp(Op.Event event) {
+	public void onNickChange(NickChangeEvent event) {
 	}
 
-	public void onPart(Part.Event event) {
+	public void onNotice(NoticeEvent event) {
 	}
 
-	public void onPing(Ping.Event event) {
+	public void onOp(OpEvent event) {
 	}
 
-	public void onPrivateMessage(PrivateMessage.Event event) {
+	public void onPart(PartEvent event) {
 	}
 
-	public void onQuit(Quit.Event event) {
+	public void onPing(PingEvent event) {
 	}
 
-	public void onRemoveChannelBan(RemoveChannelBan.Event event) {
+	public void onPrivateMessage(PrivateMessageEvent event) {
 	}
 
-	public void onRemoveChannelKey(RemoveChannelKey.Event event) {
+	public void onQuit(QuitEvent event) {
 	}
 
-	public void onRemoveChannelLimit(RemoveChannelLimit.Event event) {
+	public void onRemoveChannelBan(RemoveChannelBanEvent event) {
 	}
 
-	public void onRemoveInviteOnly(RemoveInviteOnly.Event event) {
+	public void onRemoveChannelKey(RemoveChannelKeyEvent event) {
 	}
 
-	public void onRemoveModerated(RemoveModerated.Event event) {
+	public void onRemoveChannelLimit(RemoveChannelLimitEvent event) {
 	}
 
-	public void onRemoveNoExternalMessages(RemoveNoExternalMessages.Event event) {
+	public void onRemoveInviteOnly(RemoveInviteOnlyEvent event) {
 	}
 
-	public void onRemovePrivate(RemovePrivate.Event event) {
+	public void onRemoveModerated(RemoveModeratedEvent event) {
 	}
 
-	public void onRemoveSecret(RemoveSecret.Event event) {
+	public void onRemoveNoExternalMessages(RemoveNoExternalMessagesEvent event) {
 	}
 
-	public void onRemoveTopicProtection(RemoveTopicProtection.Event event) {
+	public void onRemovePrivate(RemovePrivateEvent event) {
 	}
 
-	public void onServerPing(ServerPing.Event event) {
+	public void onRemoveSecret(RemoveSecretEvent event) {
 	}
 
-	public void onServerResponse(ServerResponse.Event event) {
+	public void onRemoveTopicProtection(RemoveTopicProtectionEvent event) {
 	}
 
-	public void onSetChannelBan(SetChannelBan.Event event) {
+	public void onServerPing(ServerPingEvent event) {
 	}
 
-	public void onSetChannelKey(SetChannelKey.Event event) {
+	public void onServerResponse(ServerResponseEvent event) {
 	}
 
-	public void onSetChannelLimit(SetChannelLimit.Event event) {
+	public void onSetChannelBan(SetChannelBanEvent event) {
 	}
 
-	public void onSetInviteOnly(SetInviteOnly.Event event) {
+	public void onSetChannelKey(SetChannelKeyEvent event) {
 	}
 
-	public void onSetModerated(SetModerated.Event event) {
+	public void onSetChannelLimit(SetChannelLimitEvent event) {
 	}
 
-	public void onSetNoExternalMessages(SetNoExternalMessages.Event event) {
+	public void onSetInviteOnly(SetInviteOnlyEvent event) {
 	}
 
-	public void onSetPrivate(SetPrivate.Event event) {
+	public void onSetModerated(SetModeratedEvent event) {
 	}
 
-	public void onSetSecret(SetSecret.Event event) {
+	public void onSetNoExternalMessages(SetNoExternalMessagesEvent event) {
 	}
 
-	public void onSetTopicProtection(SetTopicProtection.Event event) {
+	public void onSetPrivate(SetPrivateEvent event) {
 	}
 
-	public void onTime(Time.Event event) {
+	public void onSetSecret(SetSecretEvent event) {
 	}
 
-	public void onTopic(Topic.Event event) {
+	public void onSetTopicProtection(SetTopicProtectionEvent event) {
 	}
 
-	public void onUnknown(Unknown.Event event) {
+	public void onTime(TimeEvent event) {
 	}
 
-	public void onUserList(UserList.Event event) {
+	public void onTopic(TopicEvent event) {
 	}
 
-	public void onUserMode(UserMode.Event event) {
+	public void onUnknown(UnknownEvent event) {
 	}
 
-	public void onVersion(Version.Event event) {
+	public void onUserList(UserListEvent event) {
 	}
 
-	public void onVoice(Voice.Event event) {
+	public void onUserMode(UserModeEvent event) {
 	}
 
-	public void onIncomingFileTransfer(Event event) {
+	public void onVersion(VersionEvent event) {
 	}
 
-	public void onMotd(Motd.Event event) {
+	public void onVoice(VoiceEvent event) {
 	}
 }
