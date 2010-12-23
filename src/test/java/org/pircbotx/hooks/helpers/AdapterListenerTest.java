08d0d0a2dad4643d1a785541114189bf13e3e028
Leon Blakey <lord.quackstar@gmail.com>
2010-12-22 21:48:39 -0500

    Moved all test classes out of "test" subpackage

--- src/test/java/org/pircbotx/hooks/helpers/AdapterListenerTest.java
+++ src/test/java/org/pircbotx/hooks/helpers/AdapterListenerTest.java
@@ -17,7 +17,7 @@
  * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package org.pircbotx.hooks.helpers.test;
+package org.pircbotx.hooks.helpers;
 
 import java.io.File;
 import java.net.URL;
