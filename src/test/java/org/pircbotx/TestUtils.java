13a37434990c2ebf8c0d317fec670aa43abe8baf
Leon Blakey <lord.quackstar@gmail.com>
2010-12-24 17:58:32 -0500

    Moved wrapClass to TestUtils

--- src/test/java/org/pircbotx/TestUtils.java
+++ src/test/java/org/pircbotx/TestUtils.java
@@ -18,4 +18,8 @@ public class TestUtils {
 	public static String genMethod(Method method, String prefix, Class clazz) {
 		return "Method " + method.toString() + " of " + prefix + " " + clazz.getEnclosingClass().getSimpleName();
 	}
+	
+	public static String wrapClass(Class aClass, String message) {
+		return message + " in class " + aClass.toString();
+	}
 }
