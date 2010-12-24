7c81399d250ec6eb661452a79b5a1f5ac212a7b0
Leon Blakey <lord.quackstar@gmail.com>
2010-12-24 18:03:00 -0500

    Deleted genClass and genMethod

--- src/test/java/org/pircbotx/TestUtils.java
+++ src/test/java/org/pircbotx/TestUtils.java
@@ -9,16 +9,6 @@ import java.lang.reflect.Method;
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public class TestUtils {
-	public static String genClass(String prefix, Class clazz) {
-		if(clazz.getEnclosingClass() != null)
-			clazz = clazz.getEnclosingClass();
-		return prefix + " " + clazz.getSimpleName();
-	}
-
-	public static String genMethod(Method method, String prefix, Class clazz) {
-		return "Method " + method.toString() + " of " + prefix + " " + clazz.getEnclosingClass().getSimpleName();
-	}
-	
 	public static String wrapClass(Class aClass, String message) {
 		return message + " in class " + aClass.toString();
 	}
