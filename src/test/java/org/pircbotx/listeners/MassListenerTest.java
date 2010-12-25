62140df7fad1a3e3b6f4968bfc3d690e04ff7ad7
Leon Blakey <lord.quackstar@gmail.com>
2010-12-24 19:23:35 -0500

    Removed unneeded import

--- src/test/java/org/pircbotx/listeners/MassListenerTest.java
+++ src/test/java/org/pircbotx/listeners/MassListenerTest.java
@@ -23,7 +23,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.apache.commons.lang.StringUtils;
 import org.pircbotx.TestUtils;
 import org.pircbotx.hooks.helpers.ListenerAdapterInterface;
 import static org.testng.Assert.*;
