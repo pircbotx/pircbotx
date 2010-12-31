3d2e28123749bcf8570020928e69ec566f2f363b
Leon Blakey <lord.quackstar@gmail.com>
2010-12-31 00:47:59 -0500

    Added Event DataProvider in TestUtils

--- src/test/java/org/pircbotx/TestUtils.java
+++ src/test/java/org/pircbotx/TestUtils.java
@@ -18,20 +18,43 @@
  */
 package org.pircbotx;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
+import org.pircbotx.hooks.ListenerAdapterInterface;
+import org.testng.annotations.DataProvider;
 
 /**
  *
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public class TestUtils {
+	protected static final List<Class<?>> eventClasses = new ArrayList() {
+		{
+			for (Class listenerClass : ListenerAdapterInterface.class.getInterfaces())
+				//Add listener parameter method to the list
+				add(listenerClass.getDeclaredMethods()[0].getParameterTypes()[0]);
+		}
+	};
+	
+	@DataProvider
+	protected Object[][] EventDataProvider() {
+		Object[][] params = new Object[0][0];
+		for(Class<?> event : eventClasses) {
+			ArrayUtils.add(params, new Object[]{event});
+		}
+		return params;
+	}
+	
+	
 	public static String wrapClass(Class aClass, String message) {
 		return message + " in class " + aClass.toString();
 	}
 
 	public static String getRootName(Class aClass) {
 		String name = aClass.getSimpleName();
-		
+
 		if (StringUtils.endsWith(name, "Event"))
 			return name.split("Event")[0];
 		else if (StringUtils.endsWith(name, "Listener"))
@@ -40,4 +63,6 @@ public class TestUtils {
 		//Can't get anything, error out
 		throw new IllegalArgumentException("Cannot get root of class name " + aClass.toString());
 	}
+	
+
 }
