3db18c5c9e45162126420fc1d1e8e1245b6b59b3
Leon Blakey <lord.quackstar@gmail.com>
2010-12-30 23:54:14 -0500

    Recommend LinkedListenerManager + added License Header

--- src/main/java/org/pircbotx/hooks/GenericListenerManager.java
+++ src/main/java/org/pircbotx/hooks/GenericListenerManager.java
@@ -1,3 +1,21 @@
+/**
+ * Copyright (C) 2010 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of PircBotX.
+ *
+ * PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.pircbotx.hooks;
 
 import java.util.Collections;
@@ -7,6 +25,11 @@ import java.util.Set;
  * Generic ListenerManager based off of a normal event system. This is backed
  * by {@link HookUtils#callListener(org.pircbotx.hooks.Event, org.pircbotx.hooks.Listener)}
  * and a simple HashSet. 
+ *  <p>
+ * Please note: This is a very basic and absolute type of manager. If a single 
+ * class implements multiple listeners and you remove one, you remove them all! 
+ * Therefor its  highly recommended to use {@link LinkedListenerManager} instead.
+ * This class is only provided for convince.
  *
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
