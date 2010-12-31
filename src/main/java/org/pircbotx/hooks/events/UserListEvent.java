27b652d1b69f046e3c2b7dfc7b03e07b6f7dfd11
Leon Blakey <lord.quackstar@gmail.com>
2010-12-31 00:03:18 -0500

    Removed extra space from all license declarations

--- src/main/java/org/pircbotx/hooks/events/UserListEvent.java
+++ src/main/java/org/pircbotx/hooks/events/UserListEvent.java
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.pircbotx.hooks.events;
 
 import java.util.Set;
--- src/main/java/org/pircbotx/hooks/listeners/FingerListener.java
+++ src/main/java/org/pircbotx/hooks/listeners/FingerListener.java
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.pircbotx.hooks.listeners;
 
 import org.pircbotx.hooks.events.FingerEvent;
