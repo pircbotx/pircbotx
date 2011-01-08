43907e6e65c89e856e85a7c4ac31c031837d50c7
Leon Blakey <lord.quackstar@gmail.com>
2011-01-08 15:53:41 -0500

    Updated more @author javadoc tags

--- src/main/java/org/pircbotx/exception/NickAlreadyInUseException.java
+++ src/main/java/org/pircbotx/exception/NickAlreadyInUseException.java
@@ -24,10 +24,10 @@ package org.pircbotx.exception;
  * with a user name that is already in use.
  *
  * @since   0.9
- * @author  Origionally by Paul James Mutton,
- *          <a href="http://www.jibble.org/">http://www.jibble.org/</a>
- *          <p/>Forked by Leon Blakey as part of the PircBotX project
- *          <a href="http://pircbotx.googlecode.com">http://pircbotx.googlecode.com/</a>
+ * @author  Origionally by:
+ *          <a href="http://www.jibble.org/">Paul James Mutton</a> for <a href="http://www.jibble.org/pircbot.php">PircBot</a>
+ *          <p>Forked and Maintained by in <a href="http://pircbotx.googlecode.com">PircBotX</a>:
+ *          Leon Blakey <lord.quackstar at gmail.com>
  */
 public class NickAlreadyInUseException extends IrcException {
 	/**
