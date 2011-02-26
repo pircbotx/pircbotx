53ef6f130de040b0504e14d7872fd4ca99946f8d
Leon Blakey <lord.quackstar@gmail.com>
2011-02-26 18:12:25 -0500

    Rewrote parts of ThreadedListenerManager to be more customizable

--- src/main/java/org/pircbotx/hooks/managers/ThreadedListenerManager.java
+++ src/main/java/org/pircbotx/hooks/managers/ThreadedListenerManager.java
@@ -1,6 +1,7 @@
-
 package org.pircbotx.hooks.managers;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -8,55 +9,71 @@ import org.pircbotx.hooks.Event;
 import org.pircbotx.hooks.Listener;
 
 /**
- *
+ * 
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public class ThreadedListenerManager implements ListenerManager {
 	protected ExecutorService pool = Executors.newCachedThreadPool();
-	protected final ListenerManager subManager;
 	protected final boolean perHook;
+	protected Set<Listener> listeners = new HashSet<Listener>();
 
 	public ThreadedListenerManager() {
-		subManager = new GenericListenerManager();
-		this.perHook = false;
+		perHook = false;
 	}
 
 	public ThreadedListenerManager(boolean perHook) {
-		subManager = new GenericListenerManager();
 		this.perHook = perHook;
 	}
 
-	public ThreadedListenerManager(ListenerManager subManager) {
-		this.subManager = subManager;
-		this.perHook = false;
+	public ThreadedListenerManager(ExecutorService pool) {
+		perHook = false;
+		this.pool = pool;
 	}
-	
-	public ThreadedListenerManager(ListenerManager subManager, boolean perHook) {
-		this.subManager = subManager;
+
+	public ThreadedListenerManager(ExecutorService pool, boolean perHook) {
 		this.perHook = perHook;
+		this.pool = pool;
 	}
 
 	public boolean addListener(Listener listener) {
-		return subManager.addListener(listener);
+		return listeners.add(listener);
 	}
 
 	public boolean removeListener(Listener listener) {
-		return subManager.removeListener(listener);
-	}
-
-	public boolean listenerExists(Listener listener) {
-		return subManager.listenerExists(listener);
+		return listeners.remove(listener);
 	}
 
 	public Set<Listener> getListeners() {
-		return subManager.getListeners();
+		return Collections.unmodifiableSet(listeners);
 	}
-	
+
+	public boolean listenerExists(Listener listener) {
+		return listeners.contains(listener);
+	}
+
 	public void dispatchEvent(final Event event) {
-		pool.submit(new Runnable() {
-			public void run() {
-				subManager.dispatchEvent(event);
-			}
-		});
+		if (perHook)
+			//For each Listener, add a new Runnable
+			for (final Listener curListener : listeners)
+				pool.submit(new Runnable() {
+					public void run() {
+						try {
+							curListener.onEvent(event);
+						} catch (Throwable t) {
+							event.getBot().logException(t);
+						}
+					}
+				});
+		else
+			pool.submit(new Runnable() {
+				public void run() {
+					try {
+						for (Listener curListener : listeners)
+							curListener.onEvent(event);
+					} catch (Throwable t) {
+						event.getBot().logException(t);
+					}
+				}
+			});
 	}
 }
