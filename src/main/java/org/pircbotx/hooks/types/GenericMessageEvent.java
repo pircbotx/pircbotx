ffa3403be37f17a929cb603368b434d1586976fc
Leon Blakey <lord.quackstar@gmail.com>
2011-03-02 11:19:59 -0500

    Added Generic Type Event interfaces for generalizing certain events

---
+++ src/main/java/org/pircbotx/hooks/types/GenericMessageEvent.java
@@ -0,0 +1,29 @@
+package org.pircbotx.hooks.types;
+
+import org.pircbotx.User;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.events.PrivateMessageEvent;
+
+/**
+ * Generic event for an incoming message from a user, whether it be a private
+ * message or a channel message. Note that if you cast directly to this interface
+ * you will not know whether to respond in the channel or the user since the
+ * channel is not part of this interface. However most PircBotX methods internally
+ * cast to the base event and send it to the correct place
+ * <p>
+ * Used in {@link MessageEvent} and {@link PrivateMessageEvent}
+ * @author Leon Blakey <lord.quackstar@gmail.com>
+ */
+public interface GenericMessageEvent extends GenericUserEvent {
+	/**
+	 * The message the user sent
+	 * @return The message
+	 */
+	public String getMessage();
+
+	/**
+	 * The user that sent the message
+	 * @return 
+	 */
+	public User getUser();
+}
