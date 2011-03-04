b164ff167415ba4d7eb5f5e2e88e5ffe2ed4d273
Leon Blakey <lord.quackstar@gmail.com>
2011-03-04 13:56:45 -0500

    New interface GenericUserModeEvent

---
+++ src/main/java/org/pircbotx/hooks/types/GenericUserModeEvent.java
@@ -0,0 +1,16 @@
+package org.pircbotx.hooks.types;
+
+import org.pircbotx.Channel;
+import org.pircbotx.hooks.events.OpEvent;
+
+/**
+ * Any user event that happens in a channel. Eg {@link OpEvent}
+ * 
+ * @author Leon Blakey <lord.quackstar at gmail.com>
+ */
+public interface GenericUserModeEvent {
+	/**
+	 * The channel that the mode changed occured in
+	 */
+	public Channel getChannel();
+}
