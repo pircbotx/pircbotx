cb5046b77b4451493c68da8e67c614cff3f75765
Leon Blakey <lord.quackstar@gmail.com>
2011-05-12 20:49:40 -0400

    Corrected version number to 1.2 Beta 2

--- src/main/java/org/pircbotx/PircBotX.java
+++ src/main/java/org/pircbotx/PircBotX.java
@@ -130,7 +130,7 @@ public class PircBotX {
 	 * The definitive version number of this release of PircBotX.
 	 * (Note: Change this before automatically building releases)
 	 */
-	public static final String VERSION = "2.0 Alpha";
+	public static final String VERSION = "1.2 Beta 2";
 	protected Socket _socket;
 	// Connection stuff.
 	protected InputThread _inputThread = null;
