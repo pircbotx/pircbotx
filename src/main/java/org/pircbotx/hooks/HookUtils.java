33030b02951320bb7336bd8caf12e788e6c0c87a
Leon Blakey <lord.quackstar@gmail.com>
2011-06-05 19:54:34 -0400

    Make HookUtils get all hooks from ListenerAdapter

--- src/main/java/org/pircbotx/hooks/HookUtils.java
+++ src/main/java/org/pircbotx/hooks/HookUtils.java
@@ -18,56 +18,9 @@
  */
 package org.pircbotx.hooks;
 
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
-import org.pircbotx.hooks.events.ActionEvent;
-import org.pircbotx.hooks.events.ChannelInfoEvent;
-import org.pircbotx.hooks.events.ConnectEvent;
-import org.pircbotx.hooks.events.DisconnectEvent;
-import org.pircbotx.hooks.events.FileTransferFinishedEvent;
-import org.pircbotx.hooks.events.FingerEvent;
-import org.pircbotx.hooks.events.IncomingChatRequestEvent;
-import org.pircbotx.hooks.events.IncomingFileTransferEvent;
-import org.pircbotx.hooks.events.InviteEvent;
-import org.pircbotx.hooks.events.JoinEvent;
-import org.pircbotx.hooks.events.KickEvent;
-import org.pircbotx.hooks.events.MessageEvent;
-import org.pircbotx.hooks.events.ModeEvent;
-import org.pircbotx.hooks.events.MotdEvent;
-import org.pircbotx.hooks.events.NickChangeEvent;
-import org.pircbotx.hooks.events.NoticeEvent;
-import org.pircbotx.hooks.events.OpEvent;
-import org.pircbotx.hooks.events.PartEvent;
-import org.pircbotx.hooks.events.PingEvent;
-import org.pircbotx.hooks.events.PrivateMessageEvent;
-import org.pircbotx.hooks.events.QuitEvent;
-import org.pircbotx.hooks.events.RemoveChannelBanEvent;
-import org.pircbotx.hooks.events.RemoveChannelKeyEvent;
-import org.pircbotx.hooks.events.RemoveChannelLimitEvent;
-import org.pircbotx.hooks.events.RemoveInviteOnlyEvent;
-import org.pircbotx.hooks.events.RemoveModeratedEvent;
-import org.pircbotx.hooks.events.RemoveNoExternalMessagesEvent;
-import org.pircbotx.hooks.events.RemovePrivateEvent;
-import org.pircbotx.hooks.events.RemoveSecretEvent;
-import org.pircbotx.hooks.events.RemoveTopicProtectionEvent;
-import org.pircbotx.hooks.events.ServerPingEvent;
-import org.pircbotx.hooks.events.ServerResponseEvent;
-import org.pircbotx.hooks.events.SetChannelBanEvent;
-import org.pircbotx.hooks.events.SetChannelKeyEvent;
-import org.pircbotx.hooks.events.SetChannelLimitEvent;
-import org.pircbotx.hooks.events.SetInviteOnlyEvent;
-import org.pircbotx.hooks.events.SetModeratedEvent;
-import org.pircbotx.hooks.events.SetNoExternalMessagesEvent;
-import org.pircbotx.hooks.events.SetPrivateEvent;
-import org.pircbotx.hooks.events.SetSecretEvent;
-import org.pircbotx.hooks.events.SetTopicProtectionEvent;
-import org.pircbotx.hooks.events.TimeEvent;
-import org.pircbotx.hooks.events.TopicEvent;
-import org.pircbotx.hooks.events.UnknownEvent;
-import org.pircbotx.hooks.events.UserListEvent;
-import org.pircbotx.hooks.events.UserModeEvent;
-import org.pircbotx.hooks.events.VersionEvent;
-import org.pircbotx.hooks.events.VoiceEvent;
 
 /**
  *
@@ -77,57 +30,15 @@ public class HookUtils {
 	private static final List<Class<? extends Event>> allEvents = new ArrayList();
 
 	static {
-		//Add all Event classes
-		allEvents.add(ActionEvent.class);
-		allEvents.add(ChannelInfoEvent.class);
-		allEvents.add(ConnectEvent.class);
-		allEvents.add(DisconnectEvent.class);
-		allEvents.add(FileTransferFinishedEvent.class);
-		allEvents.add(FingerEvent.class);
-		allEvents.add(IncomingChatRequestEvent.class);
-		allEvents.add(IncomingFileTransferEvent.class);
-		allEvents.add(InviteEvent.class);
-		allEvents.add(JoinEvent.class);
-		allEvents.add(KickEvent.class);
-		allEvents.add(MessageEvent.class);
-		allEvents.add(ModeEvent.class);
-		allEvents.add(MotdEvent.class);
-		allEvents.add(NickChangeEvent.class);
-		allEvents.add(NoticeEvent.class);
-		allEvents.add(OpEvent.class);
-		allEvents.add(PartEvent.class);
-		allEvents.add(PingEvent.class);
-		allEvents.add(PrivateMessageEvent.class);
-		allEvents.add(QuitEvent.class);
-		allEvents.add(RemoveChannelBanEvent.class);
-		allEvents.add(RemoveChannelKeyEvent.class);
-		allEvents.add(RemoveChannelLimitEvent.class);
-		allEvents.add(RemoveInviteOnlyEvent.class);
-		allEvents.add(RemoveModeratedEvent.class);
-		allEvents.add(RemoveNoExternalMessagesEvent.class);
-		allEvents.add(RemovePrivateEvent.class);
-		allEvents.add(RemoveSecretEvent.class);
-		allEvents.add(RemoveTopicProtectionEvent.class);
-		allEvents.add(ServerPingEvent.class);
-		allEvents.add(ServerResponseEvent.class);
-		allEvents.add(SetChannelBanEvent.class);
-		allEvents.add(SetChannelKeyEvent.class);
-		allEvents.add(SetChannelLimitEvent.class);
-		allEvents.add(SetInviteOnlyEvent.class);
-		allEvents.add(SetModeratedEvent.class);
-		allEvents.add(SetNoExternalMessagesEvent.class);
-		allEvents.add(SetPrivateEvent.class);
-		allEvents.add(SetSecretEvent.class);
-		allEvents.add(SetTopicProtectionEvent.class);
-		allEvents.add(TimeEvent.class);
-		allEvents.add(TopicEvent.class);
-		allEvents.add(UnknownEvent.class);
-		allEvents.add(UserListEvent.class);
-		allEvents.add(UserModeEvent.class);
-		allEvents.add(VersionEvent.class);
-		allEvents.add(VoiceEvent.class);
+		//Add all Event classes from ListenerAdapter
+		for (Method curMethod : ListenerAdapter.class.getDeclaredMethods())
+			if (curMethod.getParameterTypes().length == 1) {
+				Class parameter = curMethod.getParameterTypes()[0];
+				if (parameter.isAssignableFrom(Event.class) && parameter != Event.class)
+					allEvents.add(parameter);
+			}
 	}
-	
+
 	/**
 	 * @return the allEvents
 	 */
