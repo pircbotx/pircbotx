8c3af4bdd7d24c2b8dced763a24f26f2d927d67a
Leon Blakey <lord.quackstar@gmail.com>
2011-07-06 16:11:46 -0400

    Moved all connection tests to seperate class and depend on it working before any OutputTest runs

--- src/test/java/org/pircbotx/PircBotXConnectTest.java
+++ src/test/java/org/pircbotx/PircBotXConnectTest.java
@@ -16,36 +16,37 @@
  * You should have received a copy of the GNU General Public License
  * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package org.pircbotx;
 
-import org.apache.commons.lang.StringUtils;
-import java.io.InputStream;
-import java.io.OutputStream;
-import org.pircbotx.hooks.managers.GenericListenerManager;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.net.Socket;
 import javax.net.SocketFactory;
-import org.testng.annotations.AfterMethod;
+import org.apache.commons.lang.StringUtils;
+import org.pircbotx.hooks.managers.GenericListenerManager;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 import static org.mockito.Mockito.*;
 
 /**
- * Test the output of PircBotX. Depend on ConnectTests to check mocked sockets
+ * Do various connect tests. Note that this is in a seperate class since PircBotXOutputTest
+ * relys on a working mock implementation
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
-@Test(dependsOnGroups = "ConnectTests")
-public class PircBotXOutputTest {	
-	PircBotX bot;
-	SocketFactory socketFactory;
-	ByteArrayInputStream botIn;
-	ByteArrayOutputStream botOut;
+@Test(groups = "ConnectTests")
+public class PircBotXConnectTest {
+	protected PircBotX bot;
+	protected SocketFactory socketFactory;
+	protected ByteArrayInputStream botIn;
+	protected ByteArrayOutputStream botOut;
 	
 	@BeforeMethod
-	public void botSetup() throws Exception {
+	public void botProvider() throws Exception {
 		//Setup bot
 		bot = new PircBotX();
 		bot.setListenerManager(new GenericListenerManager());
@@ -60,11 +61,35 @@ public class PircBotXOutputTest {
 		when(socket.getOutputStream()).thenReturn(botOut);
 		socketFactory = mock(SocketFactory.class);
 		when(socketFactory.createSocket("example.com", 6667)).thenReturn(socket);
-		
+	}
+	
+	@Test
+	public void connectTest() throws Exception {
 		//Connect the bot to the socket
 		bot.connect("example.com", 6667, null, socketFactory);
 
 		//Make sure the bot is connected
 		verify(socketFactory).createSocket("example.com", 6667);
+		
+		//Verify lines
+		String[] lines = botOut.toString().split("\r\n");
+		assertEquals(lines.length, 2, "Extra line: " + StringUtils.join(lines, System.getProperty("line.separator")));
+		assertEquals(lines[0], "NICK PircBotXBot");
+		assertEquals(lines[1], "USER " + bot.getLogin() + " 8 * :" + bot.getVersion());
+	}
+	
+	@Test(dependsOnMethods = "connectTest")
+	public void connectWithPasswordTest() throws Exception {
+		//Connect the bot to the socket
+		bot.connect("example.com", 6667, "pa55w0rd", socketFactory);
+
+		//Make sure the bot is connected
+		verify(socketFactory).createSocket("example.com", 6667);
+		
+		//Verify lines
+		String[] lines = botOut.toString().split("\r\n");
+		assertEquals(lines.length, 3);
+		assertEquals(lines[0], "PASS pa55w0rd");
+		assertEquals(lines[1], "NICK PircBotXBot");
 	}
 }
