d704828b7c68c3b56d4d0bd78bf95dbef8b17030
Leon Blakey <lord.quackstar@gmail.com>
2011-07-06 19:16:06 -0400

    Moved all output test fro PircBotXTest to PircBotXOutputTest. Also heavily modified to use output stream and a simulated server

--- src/test/java/org/pircbotx/PircBotXOutputTest.java
+++ src/test/java/org/pircbotx/PircBotXOutputTest.java
@@ -18,17 +18,20 @@
  */
 package org.pircbotx;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
-import java.io.InputStream;
-import java.io.OutputStream;
 import org.pircbotx.hooks.managers.GenericListenerManager;
 import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.net.Socket;
 import javax.net.SocketFactory;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 import static org.mockito.Mockito.*;
@@ -37,13 +40,15 @@ import static org.mockito.Mockito.*;
  * Test the output of PircBotX. Depend on ConnectTests to check mocked sockets
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
-@Test(dependsOnGroups = "ConnectTests")
-public class PircBotXOutputTest {	
-	PircBotX bot;
-	SocketFactory socketFactory;
-	ByteArrayInputStream botIn;
-	ByteArrayOutputStream botOut;
-	
+//@Test(dependsOnGroups = "ConnectTests")
+public class PircBotXOutputTest {
+	protected final String aString = "I'm some super long string that has multiple words";
+	protected PircBotX bot;
+	protected SocketFactory socketFactory;
+	protected BufferedReader botOut;
+	protected User aUser;
+	protected Channel aChannel;
+
 	@BeforeMethod
 	public void botSetup() throws Exception {
 		//Setup bot
@@ -51,20 +56,109 @@ public class PircBotXOutputTest {
 		bot.setListenerManager(new GenericListenerManager());
 		bot.setNick("PircBotXBot");
 		bot.setName("PircBotXBot");
-		
+		bot.setMessageDelay(0);
+
 		//Setup stream
-		botIn = new ByteArrayInputStream("".getBytes());
-		botOut = new ByteArrayOutputStream();
+		InputStream in = new ByteArrayInputStream("".getBytes());
+		PipedOutputStream out = new PipedOutputStream();
 		Socket socket = mock(Socket.class);
-		when(socket.getInputStream()).thenReturn(botIn);
-		when(socket.getOutputStream()).thenReturn(botOut);
+		when(socket.getInputStream()).thenReturn(in);
+		when(socket.getOutputStream()).thenReturn(out);
 		socketFactory = mock(SocketFactory.class);
 		when(socketFactory.createSocket("example.com", 6667)).thenReturn(socket);
-		
+
+		//Setup ability to read from bots output
+		botOut = new BufferedReader(new InputStreamReader(new PipedInputStream(out)));
+
 		//Connect the bot to the socket
 		bot.connect("example.com", 6667, null, socketFactory);
 
 		//Make sure the bot is connected
 		verify(socketFactory).createSocket("example.com", 6667);
+
+		//Setup useful vars
+		aUser = bot.getUser("aUser");
+		aChannel = bot.getChannel("aChannel");
+	}
+
+	@Test(description = "Verify sendAction to user")
+	public void sendActionUserTest() throws Exception {
+		bot.sendAction(aUser, aString);
+		checkOutput("PRIVMSG aUser :\u0001ACTION " + aString + "\u0001");
+	}
+
+	@Test(description = "Verify sendAction to channel")
+	public void sendActionChannelTest() throws Exception {
+		bot.sendAction(aChannel, aString);
+		checkOutput("PRIVMSG #aChannel :\u0001ACTION " + aString + "\u0001");
+	}
+
+	@Test(description = "Verify sendCTCPCommand to user")
+	public void sendCTCPCommandTest() throws Exception {
+		bot.sendCTCPCommand(aUser, aString);
+		checkOutput("PRIVMSG #aChannel :\u0001" + aString + "\u0001");
+	}
+
+	@Test(description = "Verify sendCTCPResponse to user")
+	public void sendCTCPResponseTest() throws Exception {
+		bot.sendCTCPResponse(aUser, aString);
+		checkOutput("NOTICE #aChannel :\u0001" + aString + "\u0001");
+	}
+
+	@Test(description = "Verify sendInvite to user")
+	public void sendInviteUserTest() throws Exception {
+		bot.sendInvite(aUser, aChannel);
+		checkOutput("INVITE aUser :#aChannel");
+	}
+
+	@Test(description = "Verify sendInvite to channel")
+	public void sendInviteChannelTest() throws Exception {
+		bot.sendInvite(aChannel, bot.getChannel("#otherChannel"));
+		checkOutput("INVITE #aChannel :#otherChannel");
+	}
+
+	@Test(description = "Verify sendMessage to channel")
+	public void sendMessageChannelTest() throws Exception {
+		bot.sendMessage(aChannel, aString);
+		checkOutput("PRIVMSG #aChannel :" + aString);
+	}
+
+	@Test(description = "Verify sendMessage to user")
+	public void sendMessageUserTest() throws Exception {
+		bot.sendMessage(aUser, aString);
+		checkOutput("PRIVMSG aUser :" + aString);
+	}
+
+	@Test(description = "Verify sendNotice to channel")
+	public void sendNoiceChannelTest() throws Exception {
+		bot.sendNotice(aChannel, aString);
+		checkOutput("NOTICE #aChannel :" + aString);
+	}
+
+	@Test(description = "Verify sendNotice to user")
+	public void sendNoiceUserTest() throws Exception {
+		bot.sendNotice(aUser, aString);
+		checkOutput("NOTICE #aUser :" + aString);
+	}
+
+	/**
+	 * Check the output for one line that equals the expected value.
+	 * @param expected 
+	 */
+	protected void checkOutput(String expected) throws IOException {
+		//Handle the first 2 lines from the bot
+		System.out.println("Reading first line");
+		assertEquals(botOut.readLine(), "NICK PircBotXBot", "Unexecpted first line");
+		System.out.println("Reading second line");
+		String line = botOut.readLine();
+		assertNotNull(line, "Second output line is null");
+		assertTrue(line.startsWith("USER PircBotX 8 * :"), "Unexpected second line: " + line);
+		
+		//Make sure the remaining line is okay
+		System.out.println("Reading third line");
+		line = botOut.readLine();
+		System.out.println("Finished reading lines");
+		//assertEquals(lines.length, 1, "Too many/few lines: " + StringUtils.join(lines, System.getProperty("line.separator")));
+		assertEquals(line, expected);
 	}
 }
