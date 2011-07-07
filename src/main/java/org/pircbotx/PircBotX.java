edb75ad418447eea5c072e5ee387c700797cf2f4
Leon Blakey <lord.quackstar@gmail.com>
2011-07-07 13:32:20 -0400

    Updated version number to correct version

--- src/main/java/org/pircbotx/PircBotX.java
+++ src/main/java/org/pircbotx/PircBotX.java
@@ -134,7 +134,7 @@ public class PircBotX {
 	 * The definitive version number of this release of PircBotX.
 	 * (Note: Change this before automatically building releases)
 	 */
-	public static final String VERSION = "1.4";
+	public static final String VERSION = "1.3 Beta 3";
 	protected Socket _socket;
 	// Connection stuff.
 	protected InputThread _inputThread = null;
