606ccfad34b24dc6f3e61cf9f456e0014bc69926
lordquackstar@localhost <lordquackstar@localhost>
2011-08-11 17:15:26 -0400

    Bumped version number in PircBotX

--- src/main/java/org/pircbotx/PircBotX.java
+++ src/main/java/org/pircbotx/PircBotX.java
@@ -134,7 +134,7 @@ public class PircBotX {
 	 * The definitive version number of this release of PircBotX.
 	 * (Note: Change this before automatically building releases)
 	 */
-	public static final String VERSION = "1.5";
+	public static final String VERSION = "1.6-SNAPSHOT";
 	protected Socket _socket;
 	// Connection stuff.
 	protected InputThread _inputThread = null;
