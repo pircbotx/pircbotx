d92995fff15b0bbb990d18669ce61020e56a0850
lordquackstar <lordquackstar@localhost>
2011-08-13 03:21:03 -0400

    Issue #25: Comprehensive test for ip2integer, checks various IPv4 and IPv6 addresses

--- src/test/java/org/pircbotx/PircBotXTest.java
+++ src/test/java/org/pircbotx/PircBotXTest.java
@@ -18,13 +18,17 @@
  */
 package org.pircbotx;
 
+import java.net.InetAddress;
 import java.lang.reflect.Method;
+import java.math.BigInteger;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.apache.commons.lang.StringUtils;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import static org.testng.Assert.*;
 
@@ -34,7 +38,7 @@ import static org.testng.Assert.*;
  *
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
-@Test(singleThreaded=true)
+@Test(singleThreaded = true)
 public class PircBotXTest {
 	final String aString = "I'm some super long string that has multiple words";
 
@@ -82,7 +86,7 @@ public class PircBotXTest {
 			assertTrue(requiredClasses.isEmpty(), "Method group " + key + " doesn't have a method(s) for " + StringUtils.join(requiredClasses, ", "));
 		}
 	}
-	
+
 	@Test(description = "Make sure getUser doesn't return null and reliably returns the correct value")
 	public void getUserTest() {
 		PircBotX smallBot = new PircBotX();
@@ -90,7 +94,7 @@ public class PircBotXTest {
 		assertNotNull(origUser, "getUser returns null for unknown user");
 		assertEquals(origUser, smallBot.getUser("SomeUser"), "getUser doesn't return the same user during second call");
 	}
-	
+
 	@Test(description = "Make sure channel doesn't return null and reliably returns the correct value")
 	public void getChannelTest() {
 		PircBotX smallBot = new PircBotX();
@@ -98,7 +102,7 @@ public class PircBotXTest {
 		assertNotNull(channel, "getchannel returns null for unknown channel");
 		assertEquals(channel, smallBot.getChannel("#aChannel"), "getChannel doesn't return the same channel during second call");
 	}
-	
+
 	@Test(dependsOnMethods = "getUserTest", description = "Make sure userExists works")
 	public void userExistsTest() {
 		PircBotX smallBot = new PircBotX();
@@ -107,7 +111,7 @@ public class PircBotXTest {
 		//Make sure it exists
 		smallBot.userExists("SomeUser");
 	}
-	
+
 	@Test(dependsOnMethods = "getChannelTest", description = "Make sure channelExists works")
 	public void channelExistsTest() {
 		PircBotX smallBot = new PircBotX();
@@ -116,7 +120,41 @@ public class PircBotXTest {
 		//Make sure it exists
 		smallBot.channelExists("#aChannel");
 	}
-	
+
+	@Test(dataProvider = "ipToLongDataProvider")
+	public void ipToLong(String ipAddress, BigInteger expectedResult) throws UnknownHostException {
+		PircBotX smallBot = new PircBotX();
+		
+		//First, convert to a byte array
+		InetAddress address = InetAddress.getByName(ipAddress);
+		byte[] byteIp = address.getAddress();
+		System.out.println("IP: " + ipAddress);
+		System.out.println("BigInt: " + new BigInteger(byteIp));
+		System.out.println("BigInt1: " + new BigInteger(1,byteIp));
+		System.out.println("Array Length: " + byteIp.length);
+		
+		//Next, extract the value
+		BigInteger ipToLong = smallBot.ipToInteger(address);
+		System.out.println("Bot Convert: " + ipToLong);
+		
+		
+		assertEquals(ipToLong, expectedResult, "Converting " + ipAddress + " to long is wrong");
+	}
+
+	@DataProvider
+	public Object[][] ipToLongDataProvider() {
+		//Note: All numbers are verified with another tool
+		return new Object[][]{
+					//IPv4 Tests	
+					{"127.0.0.1", new BigInteger("2130706433")},
+					{"192.168.21.6", new BigInteger("3232240902")},
+					{"75.221.45.21", new BigInteger("1272786197")},
+					//IPv6 tests
+					{"2001:0db8:85a3:0000:0000:8a2e:0370:7334", new BigInteger("42540766452641154071740215577757643572")},
+					{"fe80:0:0:0:202:b3ff:fe1e:8329", new BigInteger("338288524927261089654163772891438416681")},
+					{"fe80::202:b3ff:fe1e:5329", new BigInteger("338288524927261089654163772891438404393")},};
+	}
+
 	@Test(description = "Verify getting the bots own user object works")
 	public void getUserBotTest() {
 		PircBotX smallBot = new PircBotX();
