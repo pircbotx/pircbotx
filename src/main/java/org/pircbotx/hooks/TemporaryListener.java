179f29bba5a80c4433d338bbc9469a13af2843f7
Leon Blakey <lord.quackstar@gmail.com>
2011-08-25 19:17:17 -0400

    New class TemporaryListener

---
+++ src/main/java/org/pircbotx/hooks/TemporaryListener.java
@@ -0,0 +1,33 @@
+package org.pircbotx.hooks;
+
+import org.pircbotx.PircBotX;
+
+/**
+ * Listener that provides an easy way to make temporary listeners that aren't
+ * needed after one use
+ * <p>
+ * Your listener will only get called if the bot that generated the event matches
+ * the given one. Then when you are finished, call {@link #done()} and the listener
+ * will be removed.
+ * <p>
+ * Note: The reason the {@link #done()} method is used instead of automatically
+ * removing is that you may need to check for something before executing. 
+ * @author Leon Blakey <lord.quackstar at gmail.com>
+ */
+public class TemporaryListener extends ListenerAdapter {
+	protected final PircBotX bot;
+
+	public TemporaryListener(PircBotX bot) {
+		this.bot = bot;
+	}
+
+	@Override
+	public void onEvent(Event event) throws Exception {
+		if (event.getBot() == bot)
+			super.onEvent(event);
+	}
+
+	public void done() {
+		bot.getListenerManager().removeListener(this);
+	}
+}
