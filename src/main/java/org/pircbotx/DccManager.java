d3f665079d6c87ecda7860204c026a375a18534b
Leon Blakey <lord.quackstar@gmail.com>
2011-09-15 17:18:33 -0400

    Added utility method to generate an InetAddress from a raw string address

--- src/main/java/org/pircbotx/DccManager.java
+++ src/main/java/org/pircbotx/DccManager.java
@@ -142,9 +142,35 @@ public class DccManager {
 	void removeAwaitingResume(DccFileTransfer transfer) {
 		awaitingResume.removeElement(transfer);
 	}
-	
+
 	public static BigInteger addressToInteger(InetAddress address) {
 		return new BigInteger(1, address.getAddress());
+	}
+
+	public static InetAddress integerToAddress(String rawInteger) {
+		//Convert the rawInteger into something usable
+		BigInteger bigIp = new BigInteger(rawInteger);
+		byte[] addressBytes = bigIp.toByteArray();
+
+		//If there aren't enough bytes, pad with 0 byte	
+		if (addressBytes.length == 5)
+			//Has signum, strip it
+			addressBytes = Arrays.copyOfRange(addressBytes, 1, 5);
+		else if (addressBytes.length < 4) {
+			byte[] newAddressBytes = new byte[4];
+			newAddressBytes[3] = addressBytes[0];
+			newAddressBytes[2] = (addressBytes.length > 1) ? addressBytes[1] : (byte) 0;
+			newAddressBytes[1] = (addressBytes.length > 2) ? addressBytes[2] : (byte) 0;
+			newAddressBytes[0] = (addressBytes.length > 3) ? addressBytes[3] : (byte) 0;
+			addressBytes = newAddressBytes;
+		} else if (addressBytes.length == 17)
+			//Has signum, strip it
+			addressBytes = Arrays.copyOfRange(addressBytes, 1, 17);
+		try {
+			return InetAddress.getByAddress(addressBytes);
+		} catch (UnknownHostException ex) {
+			throw new RuntimeException("Can't get InetAdrress version of int IP address " + rawInteger + " (bytes: " + Arrays.toString(addressBytes) + ")", ex);
+		}
 	}
 
 	/**
