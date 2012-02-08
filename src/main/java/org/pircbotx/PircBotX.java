997b81ba074b706039d3950903766a7ff9d40faa
Leon Blakey <lord.quackstar@gmail.com>
2012-02-08 12:02:14 -0500

    Bumped version number in PircBotX class

--- src/main/java/org/pircbotx/PircBotX.java
+++ src/main/java/org/pircbotx/PircBotX.java
@@ -133,7 +133,7 @@ public class PircBotX {
 	 * The definitive version number of this release of PircBotX.
 	 * (Note: Change this before automatically building releases)
 	 */
-	public static final String VERSION = "1.6";
+	public static final String VERSION = "1.7";
 	protected Socket _socket;
 	// Connection stuff.
 	protected InputThread _inputThread = null;
