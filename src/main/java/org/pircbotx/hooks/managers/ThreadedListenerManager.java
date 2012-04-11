0c4cce970b698decdc01f265fdb035975f6df7de
Leon Blakey <lord.quackstar@gmail.com>
2012-04-11 12:44:51 -0400

    Fixed License header issues

--- src/main/java/org/pircbotx/hooks/managers/ThreadedListenerManager.java
+++ src/main/java/org/pircbotx/hooks/managers/ThreadedListenerManager.java
@@ -10,11 +10,11 @@
  *
  * PircBotX is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.pircbotx.hooks.managers;
 
