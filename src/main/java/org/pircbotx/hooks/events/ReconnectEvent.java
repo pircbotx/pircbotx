643c1b1aac60b1ac478bda13a69742d54d760947
Leon Blakey <lord.quackstar@gmail.com>
2012-08-09 14:51:12 -0400

    File header for ReconnectEvent

--- src/main/java/org/pircbotx/hooks/events/ReconnectEvent.java
+++ src/main/java/org/pircbotx/hooks/events/ReconnectEvent.java
@@ -1,6 +1,20 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2010-2011 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of PircBotX.
+ *
+ * PircBotX is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PircBotX is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with PircBotX.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.pircbotx.hooks.events;
 
@@ -15,9 +29,10 @@ import org.pircbotx.hooks.Event;
  */
 @Data
 @EqualsAndHashCode(callSuper = true)
-public class ReconnectEvent<T extends PircBotX> extends Event<T>  {
+public class ReconnectEvent<T extends PircBotX> extends Event<T> {
 	protected boolean success;
 	protected Exception ex;
+
 	/**
 	 * Default constructor to setup object. Timestamp is automatically set
 	 * to current time as reported by {@link System#currentTimeMillis() }
