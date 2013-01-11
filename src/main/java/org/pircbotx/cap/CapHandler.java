c161e1dcc71abb2bd7fef7ad2607e154befd14f3
Leon Blakey <lord.quackstar@gmail.com>
2013-01-11 18:14:58 -0500

    Allow CapHandlers to throw Exceptions

--- src/main/java/org/pircbotx/cap/CapHandler.java
+++ src/main/java/org/pircbotx/cap/CapHandler.java
@@ -8,7 +8,7 @@ import org.pircbotx.PircBotX;
  * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 public interface CapHandler {
-	public void handleLS(PircBotX bot, List<String> capabilities);
-	public void handleACK(PircBotX bot, List<String> capabilities);
-	public void handleNAK(PircBotX bot, List<String> capabilities);
+	public void handleLS(PircBotX bot, List<String> capabilities) throws Exception;
+	public void handleACK(PircBotX bot, List<String> capabilities) throws Exception;
+	public void handleNAK(PircBotX bot, List<String> capabilities) throws Exception;
 }
