bca65b35af11539ffc02e14055fe48e63ccd61fd
Leon Blakey <lord.quackstar@gmail.com>
2013-01-14 21:52:07 -0500

    Check if socket is closed in shutdown hook to prevent racing the InputThread to call shutdown and causing Exceptions from having shutdown() running twice

--- src/main/java/org/pircbotx/PircBotX.java
+++ src/main/java/org/pircbotx/PircBotX.java
@@ -2794,7 +2794,7 @@ public class PircBotX {
 				@Override
 				public void run() {
 					PircBotX thisBot = thisBotRef.get();
-					if (thisBot != null && thisBot.isConnected())
+					if (thisBot != null && thisBot.isConnected() && !thisBot.socket.isClosed())
 						try {
 							thisBot.disconnect();
 						} finally {
