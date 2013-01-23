fb9ac64aab03b947a141faa9867e4b973e46ff3f
Leon Blakey <lord.quackstar@gmail.com>
2013-01-22 20:48:15 -0500

    Add check for 451 ignoring to make sure it says CAP

--- src/main/java/org/pircbotx/PircBotX.java
+++ src/main/java/org/pircbotx/PircBotX.java
@@ -376,7 +376,7 @@ public class PircBotX {
 					else if (code.equals("439")) {
 						//EXAMPLE: PircBotX: Target change too fast. Please wait 104 seconds
 						// No action required.
-					} else if (capEnabled && code.equals("451")) {
+					} else if (capEnabled && code.equals("451") && params.get(0).equals("CAP")) {
 						//EXAMPLE: 451 CAP :You have not registered
 						//Ignore, this is from servers that don't support CAP
 					} else if (code.startsWith("5") || code.startsWith("4"))
