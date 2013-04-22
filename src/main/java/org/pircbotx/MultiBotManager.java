1503d63fab1d54ec102532069ec1dee19c97aec2
Leon Blakey <lord.quackstar@gmail.com>
2013-04-22 00:24:37 -0400

    Simplified MultiBotManager to use configuration class

--- src/main/java/org/pircbotx/MultiBotManager.java
+++ src/main/java/org/pircbotx/MultiBotManager.java
@@ -51,80 +51,7 @@ import org.pircbotx.hooks.managers.ListenerManager;
  */
 @Data
 public class MultiBotManager {
-	protected final Set<BotEntry> bots = new HashSet();
-	@Getter(AccessLevel.NONE)
-	protected final PircBotX dummyBot;
-	protected ListenerManager listenerManager;
-	protected String name;
-	protected boolean verbose;
-	protected int socketTimeout;
-	protected long messageDelay;
-	protected String login;
-	protected boolean autoNickChange;
-	protected Charset encoding;
-	protected InetAddress dcciNetAddress;
-	protected List<Integer> dccports;
-
-	/**
-	 * Setup MultiBotManager with all bots being called the specified name
-	 * @param name The name that all bots will have by default
-	 */
-	public MultiBotManager(final String givenName) {
-		this(new PircBotX() {
-			{
-				setName(givenName);
-			}
-		});
-	}
-
-	/**
-	 * Setup MultiBotManager by cloning ALL of the settings from the specified
-	 * bot
-	 * @param dummyBot The bot to clone the settings from
-	 */
-	public MultiBotManager(PircBotX dummyBot) {
-		//Mirror the values 
-		this.dummyBot = dummyBot;
-		name = dummyBot.getName();
-		listenerManager = dummyBot.getListenerManager();
-		verbose = dummyBot.isVerbose();
-		socketTimeout = dummyBot.getSocketTimeout();
-		messageDelay = dummyBot.getMessageDelay();
-		login = dummyBot.getLogin();
-		autoNickChange = dummyBot.isAutoNickChange();
-		encoding = dummyBot.getEncoding();
-		dcciNetAddress = dummyBot.getDccHandler().getDccInetAddress();
-		dccports = dummyBot.getDccHandler().getDccPorts();
-	}
-
-	/**
-	 * Create a bot using the specified hostname, 6667 for port, and no password or socketfactory
-	 * @param hostname The hostname of the server to connect to.
-	 */
-	public BotBuilder createBot(String hostname) {
-		return createBot(hostname, 6667, null, null);
-	}
-
-	/**
-	 * Attempt to connect to the specified IRC server and port number.
-	 * The onConnect method is called upon success.
-	 *
-	 * @param hostname The hostname of the server to connect to.
-	 * @param port The port number to connect to on the server.
-	 */
-	public BotBuilder createBot(String hostname, int port) throws IOException, IrcException, NickAlreadyInUseException {
-		return createBot(hostname, port, null, null);
-	}
-
-	/**
-	 * Create a bot using the specified hostname, port and socketfactory with no password
-	 * @param hostname The hostname of the server to connect to.
-	 * @param port The port number to connect to on the server.
-	 * @param socketFactory The factory to use for creating sockets, including secure sockets
-	 */
-	public BotBuilder createBot(String hostname, int port, SocketFactory socketFactory) {
-		return createBot(hostname, port, null, socketFactory);
-	}
+	protected final Map<PircBotX, BotBuilder> bots = new HashMap();
 
 	/**
 	 * Create a bot using the specified hostname, port, password, and socketfactory
@@ -133,24 +60,9 @@ public class MultiBotManager {
 	 * @param password The password to use to join the server.
 	 * @param socketFactory The factory to use for creating sockets, including secure sockets
 	 */
-	public BotBuilder createBot(String hostname, int port, String password, SocketFactory socketFactory) {
-		//Create bot with all of the global settings
-		PircBotX bot = new PircBotX();
-		bot.setListenerManager(listenerManager);
-		bot.setName(name);
-		bot.setVerbose(verbose);
-		bot.setSocketTimeout(socketTimeout);
-		bot.setMessageDelay(messageDelay);
-		bot.setLogin(login);
-		bot.setAutoNickChange(autoNickChange);
-		bot.setEncoding(encoding);
-		bot.getDccHandler().setDccInetAddress(dcciNetAddress);
-		bot.getDccHandler().getDccPorts().clear();
-		bot.getDccHandler().getDccPorts().addAll(dccports);
-
-		//Add to bot set
-		BotBuilder builder = new BotBuilder(bot);
-		bots.add(new BotEntry(bot, hostname, port, password, socketFactory, builder));
+	public BotBuilder createBot(Configuration config) {
+		BotBuilder builder = new BotBuilder(config);
+		bots.put(new PircBotX(), builder);
 		return builder;
 	}
 
@@ -161,12 +73,13 @@ public class MultiBotManager {
 	 * @throws NickAlreadyInUseException if our nick is already in use on the server.
 	 */
 	public void connectAll() throws IOException, IrcException, NickAlreadyInUseException {
-		for (BotEntry curEntry : bots) {
-			PircBotX bot = curEntry.getBot();
-			bot.connect(curEntry.getHostname(), curEntry.getPort(), curEntry.getPassword(), curEntry.getSocketFactory());
+		for (Map.Entry<PircBotX, BotBuilder> curEntry : bots.entrySet()) {
+			PircBotX bot = curEntry.getKey();
+			BotBuilder builder = curEntry.getValue();
+			bot.connect(builder.getConfig());
 
 			//Join channels
-			for (Map.Entry<String, String> curChannel : curEntry.getBuilder().getChannels().entrySet())
+			for (Map.Entry<String, String> curChannel : builder.getChannels().entrySet())
 				bot.joinChannel(curChannel.getKey(), curChannel.getValue());
 		}
 	}
@@ -175,11 +88,9 @@ public class MultiBotManager {
 	 * Disconnect all bots from their respective severs cleanly.
 	 */
 	public void disconnectAll() {
-		for (BotEntry curEntry : bots) {
-			PircBotX bot = curEntry.getBot();
+		for (PircBotX bot : bots.keySet())
 			if (bot.isConnected())
 				bot.disconnect();
-		}
 	}
 
 	/**
@@ -188,36 +99,15 @@ public class MultiBotManager {
 	 * @return An <i>unmodifiable</i> Set of bots that are being managed
 	 */
 	public Set<PircBotX> getBots() {
-		Set<PircBotX> actualBots = new HashSet();
-		for (BotEntry curEntry : bots)
-			actualBots.add(curEntry.getBot());
-		return Collections.unmodifiableSet(actualBots);
-	}
-
-	public void setEncoding(String encoding) throws UnsupportedEncodingException {
-		//Test if exception is thrown when setting encoding
-		dummyBot.setEncoding(encoding);
-
-		//Good, set value
-		this.encoding = dummyBot.getEncoding();
-	}
-
-	@Data
-	protected static class BotEntry {
-		protected final PircBotX bot;
-		protected final String hostname;
-		protected final int port;
-		protected final String password;
-		protected final SocketFactory socketFactory;
-		protected final BotBuilder builder;
+		return Collections.unmodifiableSet(bots.keySet());
 	}
 
 	@RequiredArgsConstructor
 	public static class BotBuilder {
 		@Getter
-		protected final PircBotX bot;
+		protected final Configuration config;
 		@Getter(AccessLevel.PROTECTED)
-		protected Map<String, String> channels = new HashMap();
+		protected final Map<String, String> channels = new HashMap();
 
 		public BotBuilder addChannel(String channelName) {
 			channels.put(channelName, "");
