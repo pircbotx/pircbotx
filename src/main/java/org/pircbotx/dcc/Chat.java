71be0cf69fc47e909bdfeaf3fc119583eba076cf
Leon Blakey <lord.quackstar@gmail.com>
2013-04-25 10:58:33 -0400

    Finished Chat interface

---
+++ src/main/java/org/pircbotx/dcc/Chat.java
@@ -0,0 +1,18 @@
+
+package org.pircbotx.dcc;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.net.Socket;
+import org.pircbotx.User;
+
+/**
+ *
+ * @author Leon
+ */
+public interface Chat {
+	public User getUser();
+	public Socket getSocket();
+	public BufferedReader getBufferedReader();
+	public BufferedWriter getBufferedWriter();
+}
