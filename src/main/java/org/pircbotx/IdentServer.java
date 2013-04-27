9e5701df1ce7eeb162a5d4db5490039cf711a443
Leon Blakey <lord.quackstar@gmail.com>
2013-04-27 07:04:26 -0400

    Rewrote IdentServer

--- src/main/java/org/pircbotx/IdentServer.java
+++ src/main/java/org/pircbotx/IdentServer.java
@@ -20,11 +20,24 @@ package org.pircbotx;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.Closeable;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.Synchronized;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * A simple IdentServer (also know as "The Identification Protocol").
@@ -38,23 +51,34 @@ import lombok.extern.slf4j.Slf4j;
  * So when a PircBotX is run on a machine that does not run an ident server,
  * it may be necessary to provide a "faked" response by starting up its
  * own ident server and sending out apparently correct responses.
- * <p>
- * An instance of this class can be used to start up an ident server
- * only if it is possible to do so. Reasons for not being able to do
- * so are if there is already an ident server running on port 113, or
- * if you are running as an unprivileged user who is unable to create
- * a server socket on that port number.
  *
  * @since PircBot 0.9c
- * @author Origionally by:
- * <a href="http://www.jibble.org/">Paul James Mutton</a> for <a href="http://www.jibble.org/pircbot.php">PircBot</a>
- * <p>Forked and Maintained by Leon Blakey <lord.quackstar at gmail.com> in <a href="http://pircbotx.googlecode.com">PircBotX</a>
+ * @author Leon Blakey <lord.quackstar at gmail.com>
  */
 @Slf4j
-public class IdentServer extends Thread {
-	private PircBotX bot;
-	private String login;
-	private ServerSocket ss = null;
+public class IdentServer implements Closeable, Runnable {
+	@Setter(AccessLevel.PROTECTED)
+	@Getter(AccessLevel.PROTECTED)
+	protected static IdentServer server;
+	protected static final Object instanceCreateLock = new Object();
+	protected final ServerSocket serverSocket;
+	protected final List<IdentEntry> identEntries = new ArrayList();
+	protected Thread runningThread;
+
+	@Synchronized("instanceCreateLock")
+	public static void startServer() {
+		if (server != null)
+			throw new RuntimeException("Already created an IdentServer instance");
+		server = new IdentServer();
+	}
+
+	@Synchronized("instanceCreateLock")
+	public static void stopServer() throws IOException {
+		if (server == null)
+			throw new RuntimeException("Never created an IdentServer");
+		server.close();
+		server = null;
+	}
 
 	/**
 	 * Constructs and starts an instance of an IdentServer that will
@@ -69,19 +93,18 @@ public class IdentServer extends Thread {
 	 * @param bot The PircBotX instance that will be used to log to.
 	 * @param login The login that the ident server will respond with.
 	 */
-	protected IdentServer(PircBotX bot, String login) {
-		this.bot = bot;
-		this.login = login;
-
+	protected IdentServer() {
 		try {
-			ss = new ServerSocket(113);
-			ss.setSoTimeout(60000);
+			serverSocket = new ServerSocket(113);
 		} catch (Exception e) {
-			throw new RuntimeException("Could not start the ident server on port 113", e);
+			throw new RuntimeException("Could not create server socket for IdentServer on port 113", e);
 		}
+	}
 
-		log.debug("Ident server running on port 113 for the next 60 seconds...");
-		this.setName(this.getClass() + "-Thread");
+	public void start() {
+		runningThread = new Thread(this);
+		runningThread.setName("IdentServer");
+		runningThread.start();
 	}
 
 	/**
@@ -91,31 +114,94 @@ public class IdentServer extends Thread {
 	 */
 	public void run() {
 		try {
-			Socket socket = ss.accept();
-			socket.setSoTimeout(60000);
-
-			BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-
-			String line = reader.readLine();
-			if (line != null) {
-				log.debug("*** Ident request received: " + line);
-				line = line + " : USERID : UNIX : " + login;
-				writer.write(line + "\r\n");
-				writer.flush();
-				log.debug("*** Ident reply sent: " + line);
-				writer.close();
-			}
+			log.info("IdentServer running on port 113");
+			while (true)
+				handleNextConnection();
 		} catch (Exception e) {
 			log.error("Exception encountered when running IdentServer", e);
+		} finally {
+			try {
+				close();
+			} catch (IOException e) {
+				log.error("Cannot close IdentServer socket", e);
+			}
+		}
+	}
+
+	public void handleNextConnection() throws SocketException, IOException {
+		//Grab next connectin
+		Socket socket = serverSocket.accept();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		InetSocketAddress remoteAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+
+		//Get and validate Ident from server
+		String line = reader.readLine();
+		if (StringUtils.isBlank(line)) {
+			log.error("Ignoring connection from " + remoteAddress + ", received blank line");
+			socket.close();
+			return;
+		}
+		String[] parsedLine = StringUtils.split(line, ", ");
+		if (parsedLine.length != 2) {
+			log.error("Ignoring connection from " + remoteAddress + ", recieved unknown line: " + line);
+			socket.close();
+			return;
+		}
+		int localPort = Utils.tryParseInt(parsedLine[0], -1);
+		int remotePort = Utils.tryParseInt(parsedLine[1], -1);
+		if (localPort == -1 || remotePort == -1) {
+			log.error("Ignoring connection from " + remoteAddress + ", recieved unparsable line: " + line);
+			socket.close();
+			return;
 		}
 
-		try {
-			ss.close();
-		} catch (Exception e) {
-			log.error("Could not close IdentServer server socket", e);
+		//Grab the IdentEntry for this ident
+		log.debug("Received ident request from " + remoteAddress + ": " + line);
+		IdentEntry identEntry = null;
+		synchronized (identEntries) {
+			for (IdentEntry curIdentEntry : identEntries)
+				if (curIdentEntry.getRemoteAddress().equals(remoteAddress.getAddress())
+						&& curIdentEntry.getRemotePort() == remotePort
+						&& curIdentEntry.getLocalPort() == localPort) {
+					identEntry = curIdentEntry;
+					break;
+				}
 		}
+		if (identEntry == null) {
+			String response = localPort + ", " + remotePort + " ERROR : NO-USER";
+			log.error("Unknown ident " + line + " from " + remoteAddress + ", responding with: " + response);
+			writer.write(line + "\r\n");
+			writer.flush();
+			socket.close();
+			return;
+		}
+		
+		//Respond to correct ident entry with login
+		String response = line + " : USERID : UNIX : " + identEntry.getLogin();
+		log.debug("Responded to ident request from " + remoteAddress + " with: " + response);
+		writer.write(line + "\r\n");
+		writer.flush();
+		socket.close();
+	}
 
-		log.debug("*** The Ident server has been shut down.");
+	protected void addIdentEntry(InetAddress remoteAddress, int remotePort, int localPort, String login) {
+		synchronized (identEntries) {
+			identEntries.add(new IdentEntry(remoteAddress, remotePort, localPort, login));
+		}
+	}
+
+	@Synchronized("instanceCreateLock")
+	public void close() throws IOException {
+		serverSocket.close();
+		log.info("Closed ident server on port 113");
+	}
+
+	@Data
+	protected static class IdentEntry {
+		protected final InetAddress remoteAddress;
+		protected final int remotePort;
+		protected final int localPort;
+		protected final String login;
 	}
 }
