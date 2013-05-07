44a58dafe1f2c7583827104524b10846fbbd1324
Leon Blakey <lord.quackstar@gmail.com>
2013-05-07 14:54:11 -0400

    Moved UserToChannelMap back under UserChannelDao

--- src/main/java/org/pircbotx/UserChannelDao.java
+++ src/main/java/org/pircbotx/UserChannelDao.java
@@ -21,6 +21,7 @@ package org.pircbotx;
 import com.google.common.collect.HashBiMap;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Multimap;
 import java.io.Closeable;
 import java.util.HashSet;
 import java.util.Set;
@@ -322,5 +323,61 @@ public class UserChannelDao implements Closeable {
 		channelNameMap.clear();
 		privateUsers.clear();
 		userNickMap.clear();
+	}
+
+	/**
+	 * A many to many map of users to channels.
+	 */
+	protected static class UserChannelMap {
+		protected final Multimap<Channel, User> channelToUserMap = HashMultimap.create();
+		protected final Multimap<User, Channel> userToChannelMap = HashMultimap.create();
+
+		public void addUserToChannel(User user, Channel channel) {
+			userToChannelMap.put(user, channel);
+			channelToUserMap.put(channel, user);
+		}
+
+		public void removeUserFromChannel(User user, Channel channel) {
+			userToChannelMap.remove(user, channel);
+			channelToUserMap.remove(channel, user);
+		}
+
+		public void removeUser(User user) {
+			//Remove the user from each channel
+			for (Channel curChannel : userToChannelMap.removeAll(user))
+				channelToUserMap.remove(curChannel, user);
+		}
+
+		public void removeChannel(Channel channel) {
+			//Remove the channel from each user
+			for (User curUser : channelToUserMap.removeAll(channel))
+				//This will automatically remove the user if they have no more channels
+				userToChannelMap.remove(curUser, channel);
+		}
+
+		public ImmutableSet<User> getUsers(Channel channel) {
+			return ImmutableSet.copyOf(channelToUserMap.get(channel));
+		}
+
+		public ImmutableSet<Channel> getChannels(User user) {
+			return ImmutableSet.copyOf(userToChannelMap.get(user));
+		}
+
+		public boolean containsEntry(User user, Channel channel) {
+			return channelToUserMap.containsEntry(channel, user) && userToChannelMap.containsEntry(user, channel);
+		}
+
+		public boolean containsUser(User user) {
+			boolean channelToUserContains = channelToUserMap.containsValue(user);
+			boolean userToChannelContains = userToChannelMap.containsKey(user);
+			if (channelToUserContains != userToChannelContains)
+				throw new RuntimeException("Map inconsistent! User: " + user + " | channelToUserMap: " + channelToUserContains + " | userToChannelMap: " + userToChannelContains);
+			return channelToUserContains;
+		}
+
+		public void clear() {
+			userToChannelMap.clear();
+			channelToUserMap.clear();
+		}
 	}
 }
