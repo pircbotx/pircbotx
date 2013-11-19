b68a188576f8e008ad9b42a4d372cb860cc8230c
Leon Blakey <lord.quackstar@gmail.com>
2013-11-19 12:54:57 -0500

    Disable test as fix is pushed to 2.1

--- src/test/java/org/pircbotx/InputParserTest.java
+++ src/test/java/org/pircbotx/InputParserTest.java
@@ -660,7 +660,8 @@ public class InputParserTest {
 		assertFalse(aChannel.hasVoice(otherUser), "User is labeled as voiced even though specified as one in WHO");
 	}
 	
-	@Test(dependsOnMethods = "whoTest", description = "Veryfy that we don't falsely registers all WHO responses as valid channels")
+	//TODO: Issue #148: Re-enable in 2.1
+	@Test(dependsOnMethods = "whoTest", description = "Veryfy that we don't falsely registers all WHO responses as valid channels", enabled = false)
 	public void whoTestFalseChannels() throws IOException, IrcException {
 		assertFalse(dao.channelExists("#randomChannel"), "Intial test to ensure channel doesn't exist");
 		
