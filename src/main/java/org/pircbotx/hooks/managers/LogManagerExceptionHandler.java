9b2749b72415f821161fa24283af87fc1d737aac
Leon Blakey <lord.quackstar@gmail.com>
2013-12-05 11:34:29 -0500

    Added ListenerManager exception handler

---
+++ src/main/java/org/pircbotx/hooks/managers/LogManagerExceptionHandler.java
@@ -0,0 +1,16 @@
+package org.pircbotx.hooks.managers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.pircbotx.hooks.Event;
+import org.pircbotx.hooks.Listener;
+
+/**
+ * Write all exceptions to log
+ * @author Leon Blakey <lord.quackstar at gmail.com>
+ */
+@Slf4j
+public class LogManagerExceptionHandler implements ManagerExceptionHandler {
+	public void onException(Listener listener, Event event, Throwable exception) {
+		log.error("Exception encountered when executing event " + event + " on listener " + listener, exception);
+	}
+}
