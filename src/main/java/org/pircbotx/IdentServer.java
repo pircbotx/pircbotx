9750deb512ace6f734a949602d26de9101984b66
Leon Blakey <lord.quackstar@gmail.com>
2014-05-16 10:14:48 -0400

    Formatting...

--- src/main/java/org/pircbotx/IdentServer.java
+++ src/main/java/org/pircbotx/IdentServer.java
@@ -3,18 +3,17 @@
  *
  * This file is part of PircBotX.
  *
- * PircBotX is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * PircBotX is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * PircBotX is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * PircBotX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.pircbotx;
 
@@ -43,17 +42,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 
 /**
- * A simple IdentServer (also know as "The Identification Protocol").
- * An ident server provides a means to determine the identity of a
- * user of a particular TCP connection.
+ * A simple IdentServer (also know as "The Identification Protocol"). An ident
+ * server provides a means to determine the identity of a user of a particular
+ * TCP connection.
  * <p>
- * Most IRC servers attempt to contact the ident server on connecting
- * hosts in order to determine the user's identity. A few IRC servers
- * will not allow you to connect unless this information is provided.
+ * Most IRC servers attempt to contact the ident server on connecting hosts in
+ * order to determine the user's identity. A few IRC servers will not allow you
+ * to connect unless this information is provided.
  * <p>
- * So when a PircBotX is run on a machine that does not run an ident server,
- * it may be necessary to provide a "faked" response by starting up its
- * own ident server and sending out apparently correct responses.
+ * So when a PircBotX is run on a machine that does not run an ident server, it
+ * may be necessary to provide a "faked" response by starting up its own ident
+ * server and sending out apparently correct responses.
  *
  * @since PircBot 0.9c
  * @author Leon Blakey <lord.quackstar at gmail.com>
@@ -71,24 +70,26 @@ public class IdentServer implements Closeable, Runnable {
 	protected final List<IdentEntry> identEntries = new ArrayList<IdentEntry>();
 	protected Thread runningThread;
 	protected int port;
-	
+
 	/**
 	 * Start the ident server with the systems default charset.
+	 *
 	 * @see Charset#defaultCharset()
 	 */
 	public static void startServer() {
 		startServer(Charset.defaultCharset(), null);
 	}
-	
+
 	/**
 	 * Start the ident server with the specified charset.
+	 *
 	 * @param encoding The encoding to use for connections
 	 */
 	@Synchronized("INSTANCE_CREATE_LOCK")
 	public static void startServer(Charset encoding, InetAddress localAddress) {
 		startServer(encoding, localAddress, DEFAULT_PORT);
 	}
-	
+
 	@Synchronized("INSTANCE_CREATE_LOCK")
 	protected static void startServer(Charset encoding, InetAddress localAddress, int port) {
 		if (server != null)
@@ -99,7 +100,8 @@ public class IdentServer implements Closeable, Runnable {
 
 	/**
 	 * Stop the server and clear pending ident responses.
-	 * @throws IOException 
+	 *
+	 * @throws IOException
 	 */
 	@Synchronized("INSTANCE_CREATE_LOCK")
 	public static void stopServer() throws IOException {
@@ -111,6 +113,7 @@ public class IdentServer implements Closeable, Runnable {
 
 	/**
 	 * Create an ident server on port 113 with the specified encoding
+	 *
 	 * @param encoding Encoding to use for sockets
 	 */
 	protected IdentServer(Charset encoding, InetAddress localAddress, int port) {
@@ -135,7 +138,7 @@ public class IdentServer implements Closeable, Runnable {
 
 	/**
 	 * Waits for a client to connect to the ident server before making an
-	 * appropriate response. 
+	 * appropriate response.
 	 */
 	public void run() {
 		log.info("IdentServer running on port " + serverSocket.getLocalPort());
@@ -147,15 +150,15 @@ public class IdentServer implements Closeable, Runnable {
 				BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), encoding));
 				OutputStreamWriter writer = new OutputStreamWriter(socket.getOutputStream(), encoding);
 				InetSocketAddress remoteAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-				
+
 				//Read first line and process
 				String line = reader.readLine();
 				String response = handleNextConnection(remoteAddress, line);
-				if(response != null) {
+				if (response != null) {
 					writer.write(response);
 					writer.flush();
 				}
-			} catch(Exception e) {
+			} catch (Exception e) {
 				if (serverSocket.isClosed()) {
 					log.debug("Server socket closed, exiting connection loop");
 					return;
@@ -165,16 +168,16 @@ public class IdentServer implements Closeable, Runnable {
 			} finally {
 				//Close user socket
 				try {
-					if(socket != null)
+					if (socket != null)
 						socket.close();
-				} catch(IOException e) {
+				} catch (IOException e) {
 					throw new RuntimeException("Exception encountered when closing user socket");
 				}
 			}
 		}
-		
+
 		//Done with connection loop, can safely close the socket now
-		if(!serverSocket.isClosed())
+		if (!serverSocket.isClosed())
 			try {
 				close();
 			} catch (IOException e) {
@@ -184,6 +187,7 @@ public class IdentServer implements Closeable, Runnable {
 
 	/**
 	 * Wait for and process the next connection.
+	 *
 	 * @throws IOException If any error occurred during reading or writing
 	 */
 	public String handleNextConnection(InetSocketAddress remoteAddress, String line) throws IOException {
@@ -221,7 +225,7 @@ public class IdentServer implements Closeable, Runnable {
 			log.error("Unknown ident " + line + " from " + remoteAddress + ", responding with: " + response);
 			return response;
 		}
-		
+
 		//Respond to correct ident entry with login
 		String response = line + " : USERID : UNIX : " + identEntry.getLogin();
 		log.debug("Responded to ident request from " + remoteAddress + " with: " + response);
@@ -234,19 +238,19 @@ public class IdentServer implements Closeable, Runnable {
 			identEntries.add(new IdentEntry(remoteAddress, remotePort, localPort, login));
 		}
 	}
-	
+
 	protected void removeIdentEntry(InetAddress remoteAddress, int remotePort, int localPort, String login) {
 		synchronized (identEntries) {
 			log.debug("Removed ident entry for address " + remoteAddress + " on port " + remotePort + " for local port " + localPort + " for " + login);
 			for (Iterator<IdentEntry> itr = identEntries.iterator(); itr.hasNext();) {
 				IdentEntry curEntry = itr.next();
-				if(curEntry.getRemoteAddress().equals(remoteAddress) && curEntry.getRemotePort() == remotePort
+				if (curEntry.getRemoteAddress().equals(remoteAddress) && curEntry.getRemotePort() == remotePort
 						&& curEntry.getLocalPort() == localPort && curEntry.getLogin().equals(login))
 					itr.remove();
 			}
 		}
 	}
-	
+
 	protected int getPort() {
 		Preconditions.checkState(!serverSocket.isClosed(), "Server socket is not open");
 		return serverSocket.getLocalPort();
@@ -254,6 +258,7 @@ public class IdentServer implements Closeable, Runnable {
 
 	/**
 	 * Close the server socket and clear pending ident responses.
+	 *
 	 * @throws IOException If an error occured during closing
 	 */
 	@Synchronized("INSTANCE_CREATE_LOCK")
--- src/test/java/org/pircbotx/IdentServerTest.java
+++ src/test/java/org/pircbotx/IdentServerTest.java
@@ -3,20 +3,18 @@
  *
  * This file is part of PircBotX.
  *
- * PircBotX is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * PircBotX is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  *
- * PircBotX is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * PircBotX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.pircbotx;
 
 import java.io.BufferedReader;
@@ -43,58 +41,58 @@ public class IdentServerTest {
 	protected IdentServer identServer;
 	protected final String entryUserName = "WorkingIrcUser";
 	protected final InetAddress entryLocalAddress;
-	
+
 	public IdentServerTest() throws UnknownHostException {
 		entryLocalAddress = InetAddress.getByName("127.23.32.32");
 	}
-	
+
 	@BeforeMethod
 	public void setup() throws UnknownHostException {
 		//Set local address to get around build server restrictions
 		IdentServer.startServer(Charset.defaultCharset(), InetAddress.getLoopbackAddress(), 0);
 		identServer = IdentServer.getServer();
 	}
-	
+
 	@AfterMethod
 	public void cleanup() throws IOException {
 		IdentServer.stopServer();
 		identServer = null;
 	}
-	
+
 	@Test
 	public void IdentSuccessTest() throws IOException {
 		String response = executeIdentServer(6667, entryLocalAddress, 55321, 6667, 55321);
 		assertEquals(response, 55321 + ", " + 6667 + " : USERID : UNIX : " + entryUserName);
 	}
-	
+
 	@Test
 	public void IdentFailInvalidAddressTest() throws IOException, UnknownHostException {
 		String response = executeIdentServer(6667, InetAddress.getByName("127.55.55.55"), 55321, 6667, 55321);
 		assertEquals(response, 55321 + ", " + 6667 + " : ERROR : NO-USER");
 	}
-	
+
 	@Test
 	public void IdentFailInvalidRemotePortTest() throws IOException, UnknownHostException {
 		String response = executeIdentServer(6667, entryLocalAddress, 55321, 9999, 55321);
 		assertEquals(response, 55321 + ", " + 9999 + " : ERROR : NO-USER");
 	}
-	
+
 	@Test
 	public void IdentFailInvalidLocalPortTest() throws IOException, UnknownHostException {
 		String response = executeIdentServer(6667, entryLocalAddress, 55321, 6667, 65535);
 		assertEquals(response, 65535 + ", " + 6667 + " : ERROR : NO-USER");
 	}
-	
+
 	public String executeIdentServer(int entryRemotePort, InetAddress actualLocalAddress, int entryLocalPort, int sentRemotePort, int sentLocalPort) throws IOException {
 		//Pretend there's a bot connected to localhost
 		identServer.addIdentEntry(entryLocalAddress, entryRemotePort, entryLocalPort, entryUserName);
-		
+
 		//Send an ident reqeust
 		Socket socket = new Socket(InetAddress.getLoopbackAddress(), IdentServer.getServer().getPort(), actualLocalAddress, 42121);
 		OutputStreamWriter socketWriter = new OutputStreamWriter(socket.getOutputStream());
 		socketWriter.write(sentLocalPort + ", " + sentRemotePort + "\r\n");
 		socketWriter.flush();
-		
+
 		//Just grab the response
 		BufferedReader socketReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		String line = socketReader.readLine();
