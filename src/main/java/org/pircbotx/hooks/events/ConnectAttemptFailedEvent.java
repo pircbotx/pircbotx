193b1c2bac3282a1e233117e74f53a44bdea2bcd
Leon Blakey <lord.quackstar@gmail.com>
2014-08-11 09:24:02 -0400

    Removed netbeans file header
    --HG--
    branch : dev

--- src/main/java/org/pircbotx/hooks/events/ConnectAttemptFailedEvent.java
+++ src/main/java/org/pircbotx/hooks/events/ConnectAttemptFailedEvent.java
@@ -16,11 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.pircbotx.hooks.events;
 
