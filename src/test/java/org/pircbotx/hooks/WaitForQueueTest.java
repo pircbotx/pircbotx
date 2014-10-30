ee546cd969cfb54efe94874b660fd5d8b283fd14
Leon Blakey <lord.quackstar@gmail.com>
2014-10-30 17:31:47 -0400

    Add file header

--- src/test/java/org/pircbotx/hooks/WaitForQueueTest.java
+++ src/test/java/org/pircbotx/hooks/WaitForQueueTest.java
@@ -1,7 +1,19 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Copyright (C) 2010-2014 Leon Blakey <lord.quackstar at gmail.com>
+ *
+ * This file is part of PircBotX.
+ *
+ * PircBotX is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * PircBotX is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.pircbotx.hooks;
 
@@ -23,7 +35,7 @@ public class WaitForQueueTest {
 		
 		WaitForQueue queue = new WaitForQueue();
 		
-		//These should all be legal
+		//These should all be legal and compile
 		MessageEvent mevent = queue.waitFor(MessageEvent.class);
 		
 		Event event;
