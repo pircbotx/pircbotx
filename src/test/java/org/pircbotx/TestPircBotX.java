0cbec12a254d003e31a1f19680e045a39f0a553f
Leon Blakey <leon.m.blakey@gmail.com>
2015-07-21 19:42:57 -0400

    Removed netbeans header from several files

--- src/test/java/org/pircbotx/TestPircBotX.java
+++ src/test/java/org/pircbotx/TestPircBotX.java
@@ -15,11 +15,6 @@
  * You should have received a copy of the GNU General Public License along with
  * PircBotX. If not, see <http://www.gnu.org/licenses/>.
  */
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.pircbotx;
 
 import com.google.common.collect.Lists;
@@ -104,4 +99,5 @@ public class TestPircBotX extends PircBotX {
 	public <E> E getTestEvent(Class<E> clazz) {
 		return getTestEvent(clazz, clazz.getSimpleName() + " not dispatched");
 	}
+	
 }
