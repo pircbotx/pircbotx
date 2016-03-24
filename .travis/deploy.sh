0d4deb5f40eec75603effed74c0690bebe51f6ee
Leon Blakey <leon.m.blakey@gmail.com>
2016-03-24 19:40:16 -0400

    travis: Updated deploy script for for reliability, using travis env vars

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -17,23 +17,36 @@
 # PircBotX. If not, see <http://www.gnu.org/licenses/>.
 #
 
-set +x
-set +e
-mvn source:jar #deploy
+set -e
+set -x
+
+if [[ $TRAVIS_PULL_REQUEST != "false" || !($TRAVIS_BRANCH = "master" || $TRAVIS_BRANCH = "stable") ]]; then
+	echo exiting due to invalid state
+	exit 0
+fi
+
+mvn source:jar deploy
 mvn -Pcomplete-build site #site-deploy
 
 #everything is built however build may of changed repo
-git checkout master
+git checkout -- .
 
-#Seriously git?! git checkout, git reset, git pull, all with gh-pages, origin/gh-pages, remotes/origin, gh-pages?
-#Nope, obviously you do
-git fetch origin -f gh-pages:gh-pages
+# switch to gh-pages where site is stored, was not downloaded by travis though
+git fetch origin gh-pages:gh-pages
 git checkout gh-pages
-#...
-rm -rf snapshot/
-mv target/site/* snapshot/
-git add snapshot
+
+if [ "$TRAVIS_BRANCH"  == "stable" ]; then
+	SNAP_FOLDER=snapshot-stable
+else
+	# on master
+	SNAP_FOLDER=snapshot
+fi
+
+rm -rf $SNAP_FOLDER
+mkdir $SNAP_FOLDER
+mv target/site/* $SNAP_FOLDER/
+git add $SNAP_FOLDER
 git config user.name "Leon Blakey"
 git config user.email "leon.m.blakey@gmail.com"
-git commit -m "Autobuild of maven site by Travis CI"
-#git push https://$DEPLOY_PAGES_USERNAME:$DEPLOY_PAGES_PASSWORD@github.com/TheLQ/pircbotx.git
+git commit -m "Autobuild of $SNAP_FOLDER for $TRAVIS_BRANCH"
+git push https://$DEPLOY_PAGES_USERNAME:$DEPLOY_PAGES_PASSWORD@github.com/TheLQ/pircbotx.git
